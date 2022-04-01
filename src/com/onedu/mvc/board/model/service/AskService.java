00622e66d45936e39d1ce716524c2516b34c94ba
YeonjeeSong <yeonjee.song@gmail.com>
2022-04-01 10:29:53 +0900

    mypage commit

--- src/com/onedu/mvc/board/model/service/AskService.java
+++ src/com/onedu/mvc/board/model/service/AskService.java
@@ -18,13 +18,13 @@ public class AskService {
 	}
 
 	/* 마이페이지 1:1 문의 전체 조회용 메소드 */
-	public List<AskDTO> selectAllAskList() {
+	public List<AskDTO> selectAllAskList(String userId) {
 		
 		/* 1. SqlSession 생성 */
 		SqlSession sqlSession = getSqlSession();
 		
 		/* 2. SqlSession과 함께 정보를 전달하여 조회한다. */
-		List<AskDTO> askList = askDAO.selectAllAskList(sqlSession);
+		List<AskDTO> askList = askDAO.selectAllAskList(sqlSession, userId);
 		
 		/* 3. 트랜잭션 처리 - 전체 조회이기 때문에 생략 */
 		
--- src/com/onedu/mvc/board/model/service/ReviewService.java
+++ src/com/onedu/mvc/board/model/service/ReviewService.java
@@ -18,13 +18,13 @@ public class ReviewService {
 	}
 
 	/* 마이페이지 review 전체 조회용 메소드 */
-	public List<ReviewDTO> selectAllReviewList() {
+	public List<ReviewDTO> selectAllReviewList(String userId) {
 		
 		/* 1. SqlSession 생성 */
 		SqlSession sqlSession = getSqlSession();
 		
 		/* 2. SqlSession과 함께 정보를 전달하여 조회한다. */
-		List<ReviewDTO> reviewList = reviewDAO.selectAllReviewList(sqlSession);
+		List<ReviewDTO> reviewList = reviewDAO.selectAllReviewList(sqlSession, userId);
 		
 		/* 3. 트랜잭션 처리 - 전체 조회이기 때문에 생략 */
 		
